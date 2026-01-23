a2c668adf498c4c0972921d0120ffa50ed59e200
ranbhat23 <ranbhat@gmail.com>
2026-01-23 07:00:11 +0530

    fixed

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,7 +3,6 @@ import HomePage from '../pages/home.f7';
 import AboutPage from '../pages/about.f7';
 import FormPage from '../pages/form.f7';
 import githubPage from '../pages/github.f7';
-import ProductPage from '../pages/product.f7';
 import pm2statusPage from '../pages/pm2status.f7';
 
 import DynamicRoutePage from '../pages/dynamic-route.f7';
@@ -26,10 +25,6 @@ var routes = [
   {
     path: '/github/',
     component: githubPage,
-  },
-  {
-    path: '/product/:id/',
-    component: ProductPage,
   },
   {
     path: '/pm2status/',
